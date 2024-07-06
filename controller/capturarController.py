d67a8eceb6f9e51b65eff27c47b67e4ec669dd96
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2024-07-06 18:33:32 -0500

    Implementacion acceso a directorio y controlador de grabacion de audio en capturarController

--- controller/capturarController.py
+++ controller/capturarController.py
@@ -1,18 +1,34 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore import pyqtSlot
+import numpy as np
 from view.defaultView import Ui_NuevoRegistro
-from PyQt5 import QtWidgets, QtCore, QtGui
-import sys
+from model.Audio import Audio
 
 class MainWindow(QtWidgets.QMainWindow, Ui_NuevoRegistro):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.initWindow()
+        self.initAction()
+        self.show()
+        self.audio_thread = None
+
+    def initWindow(self):
         self.setWindowTitle("Nature's Symphony")
         self.hideComponents()
-        self.labelSenal.pressed.connect(self.pressMicro)
-        self.labelSenal.released.connect(self.releasedMicro)
+
+    def initAction(self):
         self.btnPlay.clicked.connect(self.pressPlay)
-        self.show()
-    
+        self.labelSenal.pressed.connect(self.pressMicro)
+        self.btnAlmacenamiento.clicked.connect(self.selectAudioFile)
+
+    def selectAudioFile(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "Seleccionar archivo de audio", "", "Audio Files (*.mp3 *.flac *.ogg *.wav *.aac *.wma)") 
+        if file_path:   
+            self.input_audio = Audio().convert_audio_to_wav(file_path)
+            self.showComponents()
+        
     def hideComponents(self):
         self.btnAceptarCaptura.hide()
         self.btnPlay.hide()
@@ -20,17 +36,25 @@ class MainWindow(QtWidgets.QMainWindow, Ui_NuevoRegistro):
         self.labelSenal.setStyleSheet("background: rgb(170, 255, 127); border-radius: 100px;image: url(./assets/images/microfono-de-estudio.png);")
 
     def pressMicro(self):
-        print("Presionando Micro...")
-    
-    def releasedMicro(self):
-        print("Cambiando interfaz...")
+        self.showComponents()
+        #Mostrar onda de audio para indicar que se esta comenzando la grabacion del audio
+        self.pressPlay()
+        #Inicializar hilo secundario para realizar grabacion
+        self.audio_thread = Audio()
+        #Se indica accion a relizar en el hilo secundario
+        self.audio_thread.audio_recorded.connect(self.on_audio_recorded)
+        self.audio_thread.start()
+
+    def showComponents(self):
         self.labelAcercar.show()
-        self.btnPlay.show()
         self.btnAceptarCaptura.show()
         self.labelSenal.setStyleSheet("background: rgb(170, 255, 127); border-radius: 100px;image: none;")
         self.btnAlmacenamiento.hide()
-    
+
+    @pyqtSlot(np.ndarray)
+    def on_audio_recorded(self, audio_data):
+        self.input_audio = audio_data
+        
     def pressPlay(self):
-        print("Reproduciendo...")
-        # Creamos una señal que queremos que se muestre en el label por 10 segundos por ahora
-        self.labelSenal.startAnimation(5)
+        # Creamos una señal que queremos que se muestre en el label por 8 segundos por ahora
+        self.labelSenal.startAnimation(6)
---
+++ model/Audio.py
@@ -0,0 +1,40 @@
+import numpy as np
+from io import BytesIO
+import sounddevice as sd
+from pydub import AudioSegment
+from PyQt5.QtCore import QThread, pyqtSignal
+
+class Audio(QThread):
+    # Instancia necesaria para mandar el audio al hilo principal de la aplicacion
+    audio_recorded = pyqtSignal(np.ndarray)
+
+    def convert_audio_to_wav(self, path_audio_file):
+        # Detectar el tipo de archivo de entrada
+        formato_entrada = path_audio_file.split('.')[-1]
+        
+        # Leer el archivo de entrada con pydub
+        audio = AudioSegment.from_file(path_audio_file, format=formato_entrada)
+        
+        # Crear un objeto BytesIO para guardar el archivo WAV en memoria
+        audio_wav = BytesIO()
+        
+        # Exportar el archivo a WAV en el objeto BytesIO
+        audio.export(audio_wav, format='wav')
+        
+        # Asegurarse de que el puntero esté al principio del archivo BytesIO
+        audio_wav.seek(0)
+        
+        return audio_wav
+    
+    def run(self):
+        #Frecuencia de muestreo usada
+        fs = 44100
+        #Duracion del audio
+        seconds = 8
+        #Grabar el audio
+        audio_data = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+        #El hilo secundaria donde se esta grabando el audio espera que acabe con la grabacion
+        sd.wait()
+        #Se devulve el resultado al hilo principal
+        self.audio_recorded.emit(audio_data)
+        
